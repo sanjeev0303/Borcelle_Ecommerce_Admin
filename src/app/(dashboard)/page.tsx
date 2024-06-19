957a7f0d212507cf27df09f2f4e285876e0afee5
sanjeev0303 <sanjeev.081021@gmail.com>
2024-06-19 23:37:53 +0530

    add clerk-authentication

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import {UserButton} from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <>
-    <div></div>
+    <div>
+      <UserButton/>
+    </div>
     </>
   );
 }
